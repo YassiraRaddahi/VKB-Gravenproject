602be517b64ed3134d12508fa52dad318d815745
YassiraRaddahi <72447342+YassiraRaddahi@users.noreply.github.com>
2026-04-01 00:01:30 +0200

    Verder met profielpagina + usergegevens uit api

--- apps/web/src/views/ProfileView.vue
+++ apps/web/src/views/ProfileView.vue
@@ -1,19 +1,70 @@
 <template>
-   <div class="d-flex justify-center mb-20">
-      <h2 class="title">
-       Profielgegevens
-      </h2>
-    </div>
+  <div class="d-flex justify-center mb-20">
+    <h2 class="title">
+      Profielgegevens
+    </h2>
+  </div>
 
-    <v-card>
-      <v-card-text>
-        <!-- Profielgegevens hier -->
-      </v-card-text>
-    </v-card>
+  <v-card color="#f08360">
+    <v-card-text>
+      <v-form v-model="valid">
+        <v-container>
+          <v-row>
+            <v-col cols="12" >
+              <v-text-field v-model="user.first_name" :counter="10" :rules="nameRules" label="Voornaam"
+                required></v-text-field>
+            </v-col>
+</v-row>
+<v-row>
+            <v-col cols="12" >
+              <v-text-field v-model="user.last_name" :counter="10" :rules="nameRules" label="Achternaam"
+                required></v-text-field>
+            </v-col>
+</v-row>
+            <v-row>
+            <v-col cols="12">
+              <v-text-field v-model="user.email" :rules="emailRules" label="E-mail" required></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="#0d475a" variant="flat">
+        Opslaan
+      </v-btn>
+    </v-card-actions>
+
+  </v-card>
 </template>
 
 <script setup>
-// logica, imports, props, reactive state
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const user = ref({})
+
+
+const url = 'http://localhost:3001/api/login'
+
+onMounted(() => {
+  axios.post(url, {
+    email: 'j.kempenaar@kerkrentmeester.nl',
+    password: 'test123'
+  })
+    .then(response => {
+      console.log(response)
+      user.value = response.data.user
+    })
+    .catch(error => {
+      console.error("Fout bij ophalen gebruikersgegevens:", error)
+    })
+}
+
+)
+
 </script>
 
 <style scoped>
