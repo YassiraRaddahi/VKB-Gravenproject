59c12c8d7df580d190cf31e96675ecdd85fa2823
BBakkerr <beabakkerr@gmail.com>
2026-04-06 13:52:34 +0200

    Added Security Router within the profile and SecurityView.vue for navigation

---
+++ apps/web/src/views/SecurityView.vue
@@ -0,0 +1,17 @@
+<template>
+ <div class="d-flex justify-center mb-20">
+    <h2 class="title">
+      Wachtwoord aanpassen
+    </h2>
+  </div>
+
+  
+</template>
+
+<script setup>
+// logica, imports, props, reactive state
+</script>
+
+<style scoped>
+/* alleen voor deze component */
+</style>
