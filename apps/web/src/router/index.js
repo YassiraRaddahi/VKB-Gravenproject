03ff5b71fd04264a75e025ecc978bff8018b3d03
Bram Meijer <brammeijer16@hotmail.com>
2026-04-11 15:20:54 +0200

    Error handeling was ik vergeten erin te doen

--- apps/web/src/router/index.js
+++ apps/web/src/router/index.js
@@ -1,42 +1,61 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import axios from 'axios'
+import { createRouter, createWebHistory } from "vue-router";
+import axios from "axios";
 
-import Home from '../views/HomeView.vue'
-import Cemeteries from '../views/CemeteriesView.vue'
-import Graves from '../views/GravesView.vue'
-import CemeteryManagers from '../views/CemeteryManagersView.vue'
-import Login from '../views/LoginView.vue'
+import Home from "../views/HomeView.vue";
+import Cemeteries from "../views/CemeteriesView.vue";
+import Graves from "../views/GravesView.vue";
+import CemeteryManagers from "../views/CemeteryManagersView.vue";
+import Login from "../views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/home' },
-    { path: '/home', name: 'Home', component: Home },
-    { path: '/login', name: 'Login', component: Login },
-    { path: '/begraafplaatsen', name: 'Cemeteries', component: Cemeteries, meta: { requiresAuth: true } },
-    { path: '/graven/:cemetery_id', name: 'Graves', component: Graves, meta: { requiresAuth: true } },
-    { path: '/beheerders', name: 'CemeteryManagers', component: CemeteryManagers, meta: { requiresAuth: true } }, 
-  ]
-})
+    { path: "/", redirect: "/home" },
+    { path: "/home", name: "Home", component: Home },
+    { path: "/login", name: "Login", component: Login },
+    {
+      path: "/begraafplaatsen",
+      name: "Cemeteries",
+      component: Cemeteries,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/graven/:cemetery_id",
+      name: "Graves",
+      component: Graves,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/beheerders",
+      name: "CemeteryManagers",
+      component: CemeteryManagers,
+      meta: { requiresAuth: true },
+    },
+  ],
+});
 
 router.beforeEach(async (to, from, next) => {
   if (to.meta.requiresAuth) {
     try {
-      await axios.get('http://localhost:3001/api/me', { withCredentials: true })
-      next()
+      await axios.get("http://localhost:3001/api/me", {
+        withCredentials: true,
+      });
+      next();
     } catch {
-      next('/login')
+      next("/login");
     }
-  } else if (to.path === '/login') {
+  } else if (to.path === "/login") {
     try {
-      await axios.get('http://localhost:3001/api/me', { withCredentials: true })
-      next('/dashboard') 
+      await axios.get("http://localhost:3001/api/me", {
+        withCredentials: true,
+      });
+      next("/dashboard");
     } catch {
-      next()
+      next();
     }
   } else {
-    next()
+    next();
   }
-})
+});
 
-export default router
+export default router;
--- apps/web/src/views/LoginView.vue
+++ apps/web/src/views/LoginView.vue
@@ -4,59 +4,49 @@
       Login
     </h2>
   </div>
-    <v-form @submit.prevent="submit">
+  <v-form @submit.prevent="submit">
 
-      <v-alert v-if="loginError" type="error" class="mb-4" > {{ loginError }} </v-alert>
 
-  <v-container class="fill-height">
-    <v-row justify="center" align="center">
-      <v-col cols="12" sm="8" md="5">
 
-        <v-card class="pa-8 rounded-xl bg-darkBlue">
+    <v-container class="fill-height">
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="8" md="5">
 
-<v-text-field
-  v-model="state.email"
-  label="E-mailadres"
-  variant="solo"
-  bg-color="white"
-  color="darkBlue"
-  rounded="xl"
-  class="mb-4"
-></v-text-field>
+          <v-card class="pa-8 rounded-xl bg-darkBlue">
+            <v-alert v-if="loginError" type="error" class="mb-4">
+              {{ loginError }}
+            </v-alert>
 
-<v-text-field
-  v-model="state.password"
-  label="Wachtwoord"
-  type="password"
-  variant="solo"
-  bg-color="white"
-  color="darkBlue"
-  rounded="xl"
-  class="mb-4"
-></v-text-field>
+            <v-text-field v-model="state.email" label="E-mailadres" variant="solo" bg-color="white" color="darkBlue"
+              rounded="xl" class="mb-6" :error-messages="v$.email.$errors.map(e => e.$message)"
+              @blur="v$.email.$touch"></v-text-field>
 
-<v-row align="center">
-  <v-col>
-    <v-btn class="rounded-xl border-white text-darkBlue text-caption">
-      Wachtwoord vergeten
-    </v-btn>
-  </v-col>
-  <v-col cols="auto">
-    <v-btn type="submit" class="rounded-xl bg-white text-darkBlue">
-      Log in
-    </v-btn>
-  </v-col>
-</v-row>
+            <v-text-field v-model="state.password" label="Wachtwoord" type="password" variant="solo" bg-color="white"
+              color="darkBlue" rounded="xl" class="mb-6" :error-messages="v$.password.$errors.map(e => e.$message)"
+              @blur="v$.password.$touch"></v-text-field>
 
-</v-card>
+            <v-row align="center">
+              <v-col>
+                <v-btn class="rounded-xl border-white text-darkBlue text-caption">
+                  Wachtwoord vergeten
+                </v-btn>
+              </v-col>
+              <v-col cols="auto">
+                <v-btn type="submit" class="rounded-xl bg-white text-darkBlue">
+                  Log in
+                </v-btn>
+              </v-col>
+            </v-row>
 
-      </v-col>
-    </v-row>
-  </v-container>
-    </v-form>
-  </template>
-  
-  <script setup>
+          </v-card>
+
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script setup>
 import { reactive, ref } from 'vue'
 import { useVuelidate } from '@vuelidate/core'
 import { email, minLength, required } from '@vuelidate/validators'
@@ -87,9 +77,9 @@ async function submit() {
   loginError.value = ''
 
   try {
-    const response = await axios.post('http://localhost:3001/api/login', 
-      {email: state.email, password: state.password,}, 
-    { withCredentials: true, }
+    const response = await axios.post('http://localhost:3001/api/login',
+      { email: state.email, password: state.password, },
+      { withCredentials: true, }
     )
 
     router.push('/dashboard')
@@ -99,8 +89,8 @@ async function submit() {
     v$.value.password.$reset()
 
     if (error.response?.status === 429) {
-        loginError.value = 'Te veel pogingen, probeer het later opnieuw'
-      } else if (error.response?.status === 401) {
+      loginError.value = 'Te veel pogingen, probeer het later opnieuw'
+    } else if (error.response?.status === 401) {
       loginError.value = 'E-mail of wachtwoord is onjuist'
     } else {
       loginError.value = 'Er is iets misgegaan, probeer opnieuw'
@@ -108,7 +98,7 @@ async function submit() {
   }
 }
 </script>
-  
-  <style scoped>
-  /* alleen voor deze component */
-  </style>
+
+<style scoped>
+/* alleen voor deze component */
+</style>
