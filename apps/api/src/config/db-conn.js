6193304b7dd3bf10d59dbd6763625c635a6964a8
YassiraRaddahi <72447342+YassiraRaddahi@users.noreply.github.com>
2026-04-27 04:02:36 +0200

    databaseconnectie credentials uit .env file ipv hardcoded

--- apps/api/src/config/db-conn.js
+++ apps/api/src/config/db-conn.js
@@ -1,10 +1,10 @@
 let mysql = require('mysql2');
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'vkb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect(function (err) {
