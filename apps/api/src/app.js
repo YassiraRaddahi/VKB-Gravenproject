d77dea362d14ef6409e0e9f979029e7ffba7abb7
YassiraRaddahi <72447342+YassiraRaddahi@users.noreply.github.com>
2026-06-01 23:01:51 +0200

    proxy op true

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const app = express();
-app.set('trust proxy', 1);
+app.set('trust proxy', true);
 
 app.use(cors({
   origin: [
